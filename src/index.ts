6ddfef5f41e455d58a55d0b60e6925d10ece213f
kalucky0 <kalucki23@gmail.com>
2022-04-07 17:47:14 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// const incrementButton = <HTMLButtonElement>document.querySelector("#increment");
+// const decrementButton = <HTMLButtonElement>document.querySelector("#decrement");
+// const countValue = <HTMLSpanElement>document.querySelector("#count-value");
+
+// const handleIncrementClick = () => {
+  // const currentValue = parseFloat(countValue.innerText);
+  // const incrementedValue = increment(currentValue);
+  // countValue.innerText = incrementedValue.toString();
+// };
+
+// const handleDecrementClick = () => {
+  // const currentValue = parseFloat(countValue.innerText);
+  // const decrementedValue = decrement(currentValue);
+  // countValue.innerText = decrementedValue.toString();
+// };
+
+// incrementButton.addEventListener("click", handleIncrementClick);
+// decrementButton.addEventListener("click", handleDecrementClick);
